87a89d9c34eb145b26db92a765cf7e6db611c778
DonQuijoTeDe <q910553@gmail.com>
2016-03-22 00:12:10 +0800

    initialization

---
+++ js/EcgGridlines.js
@@ -0,0 +1,11 @@
+'use strict'
+
+(function (exports) {
+  var EcgGridlinesManager = function() {
+
+  }
+  EcgGridlinesManager.prototype = {
+
+  };
+  exports.EcgGridlinesManager = EcgGridlinesManager;
+})(window);
---
+++ js/MainActivity.js
@@ -0,0 +1,7 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function (event) {
+    var todoecggridlines = new EcgGridlinesManager();
+    //doing the background UI
+    todomediastream.start();
+});
